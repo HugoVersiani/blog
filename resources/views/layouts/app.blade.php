8a6b83d33caf64671df471c1349194652e46077f
Hugo Versiani <hugoversiani@live.com>
2023-01-05 07:59:59 -0300

    Footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
-    <script src="https://kit.fontawesome.com/6b3f1a85d7.js" crossorigin="anonymous"></script>
+        <script src="https://kit.fontawesome.com/6b3f1a85d7.js" crossorigin="anonymous"></script>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="{{asset('css/global.css')}}">
